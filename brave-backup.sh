ee925531c8bd8a8493e821bf9f1bd7737ef3a132
Karthik Sairam <86529421+ss-karthik@users.noreply.github.com>
2021-08-09 23:12:28 +0530

    The actual scripts

---
+++ brave-backup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#brave backup script
+
+cd ~/.config
+
+tar -czvf my-brave-browser-backup.tar.gz BraveSoftware
+
+mv my-brave-browser-backup.tar.gz ~/
+
+
---
+++ brave-restore.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#restoring the brave backup that was cerated in the previous script
+
+#make sure to install Brave before running script
+
+tar xvf my-brave-browser-backup.tar.gz -C ~/.config
